a626f59398921d1f2f60f96be535f50217c79a80
Sam <samclaymichael@gmail.com>
2025-11-29 01:12:46 +0000

    Added requreAuth

--- src/main/java/org/Sam/ServerTypes.java
+++ src/main/java/org/Sam/ServerTypes.java
@@ -1,17 +1,26 @@
 package org.Sam;
 
 public enum ServerTypes {
-    HUB(100),
-    SURVIVAL(50),
-    SKYWARS(12);
+    /**
+     * Auth in this context means only specific players who reserved a slot can join
+     */
+    HUB(100, false),
+    SURVIVAL(50, false),
+    SKYWARS(12,true);
 
     private final int maxPlayers;
+    private final boolean requireAuth;
 
-    ServerTypes(int maxPlayers) {
+    ServerTypes(int maxPlayers, boolean requireAuth) {
         this.maxPlayers = maxPlayers;
+        this.requireAuth = requireAuth;
     }
 
     public int getMaxPlayers() {
         return maxPlayers;
+    }
+
+    public boolean getRequireAuth() {
+        return requireAuth;
     }
 }
